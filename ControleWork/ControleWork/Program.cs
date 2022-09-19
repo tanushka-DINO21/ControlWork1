e67b5a45a58ebadf1084a7d5595ceaa673c9e78c
Tatiana <trubina-tanya@inbox.ru>
2022-09-19 17:43:04 +0300

    Create program

---
+++ ControleWork/ControleWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ControleWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите массив строк");
+            string[] mas = Console.ReadLine().Split(' ');
+            string result = null;
+
+            for (int i = 0; i < mas.Length; i++) 
+            {
+                if (mas[i].Length <= 3)
+                {
+                    result += mas[i] + " ";
+                }
+            }
+
+            string[] res = result.Split(" ");
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.Write(res[i] + " ");
+            }
+        }
+    }
+}
